706041dbe287b799e03c88d5339376dbd4f93202
atul <atul@kaino.africa>
2023-05-16 19:02:17 +0530

    add teacher

--- src/pages/dashboard/school/create-teacher.js
+++ src/pages/dashboard/school/create-teacher.js
@@ -3,20 +3,20 @@ import React from 'react';
 import SchoolLeftNav from '@/Component/Generic/LeftNavbar/SchoolLeftNav';
 import SchoolHeader from '@/Component/Generic/Header/SchoolHeader';
 import RightContentWrapper from '@/Component/Generic/RightBarContent/RightContentWrapper';
-import SchoolCreateStudentContent from '@/Component/school/RightNavContent/students/SchoolCreateStudentContent';
+import SchoolCreateTeacherContent from '@/Component/school/RightNavContent/teachers/SchoolCreateTeacherContent';
 
-const CreateStudent = () => {
+const CreateTeacher = () => {
 
     return (
         <Layout style={{ minHeight: '100vh', maxHeight: "100vh", overflow: "hidden" }}>
             <SchoolLeftNav />
             <Layout className="site-layout" style={{ maxHeight: "100vh", overflow: 'scroll', overflowX: "hidden" }}>
-                <SchoolHeader headerText='Create Student' />
+                <SchoolHeader headerText='Create Teacher' />
                 <RightContentWrapper padding='14px'>
-                    <SchoolCreateStudentContent />
+                    <SchoolCreateTeacherContent />
                 </RightContentWrapper>
             </Layout>
         </Layout>
     );
 };
-export default CreateStudent;
+export default CreateTeacher;
